6eaa4d8faabbfb4539c9fb244bb250ce1f7a3d7c
Christiaan Kruger <christiaan@embarkmobile.com>
2014-03-26 21:53:10 +0200

    new files

---
+++ ticker/server/settings.js
@@ -0,0 +1,7 @@
+var goods = [
+	{"name" : "Toys", "price" : "5.00"},
+	{"name" : "Paper", "price" : "5.00"},
+	{"name" : "Kryptonite", "price" : "5.00"},
+	{"name" : "Gold", "price" : "5.00"},
+	{"name" : "Silver", "price" : "5.00"}
+];
